6314de8c974ecd6675e0854859eb69f82cb12f94
LihiShapir <90560670+LihiShapir@users.noreply.github.com>
2022-02-16 13:34:27 +0200

    Added load_data

--- lihi-cancer-project (1).py
+++ lihi-cancer-project (1).py
@@ -5,7 +5,8 @@ Created on Fri Jan  7 08:54:29 2022
 @author: Student
 """
 
-import directories_files
+#import BuildDataset.py as bd
+# import directories_files
 import numpy as np
 import pandas as pd
 import os
@@ -17,11 +18,10 @@ from PIL import Image
 from tqdm.notebook import tqdm
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-# import cv2
+import cv2
 
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
-
 from tensorflow.keras import Sequential
 from tensorflow.keras.optimizers import SGD, RMSprop, Adam, Adagrad, Adadelta
 from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization, Conv2D, MaxPool2D, MaxPooling2D
@@ -29,6 +29,8 @@ from tensorflow.keras.callbacks import EarlyStopping
 import keras_preprocessing.image as IMAGE
 
 import random
+
+from glob import glob
 
 random.seed(0)
 
@@ -48,54 +50,63 @@ class NN:
 		self.model.add(Dense(1,activation='sigmoid'))
 		self.model.compile(loss = 'binary_crossentropy', optimizer ='adam', metrics= ['accuracy'])		
 		self.model.summary()
-
-base = r'D:/breast cancer dataset/'
-
-ids = os.listdir(base)
-data0 = []
-data1 = []
-for id in tqdm(ids):
-  try:
-    files1 = os.listdir(base + id + '/1/')
-    files0 = os.listdir(base + id + '/0/')
-    for x in files1:
-      data1.append(base + id + '/1/' + x)
-    for x in files0:
-      data0.append(base + id + '/0/' + x)
-  except:
-    FileNotFoundError
-print(len(data1))
-print(len(data0))
-
-random.shuffle(data1)
-data1 = data1[:20000]
-len(data1)
-
-images=[]
-labels=[]
-
-for i in tqdm(data1):
-  label = int(i[-5])
-  img = IMAGE.img_to_array(IMAGE.load_img(i, target_size=(50, 50)))
-  images.append(img)
-  labels.append(label)
-
-y = np.array(labels)
-x = np.stack(images)/255
-
-print(np.unique(y, return_counts=True))
-
-x_train,x_test,y_train,y_test = train_test_split(x, y, random_state=0, test_size=0.3)
-print(x_train.shape, x_test.shape, y_train.shape, y_test.shape)
+		
+def load_data(path):
+	path = path + r'\**\*.png'
+	imagePatches = glob(path , recursive=True)
+	images = []
+	labels = []
+	
+	for i in range(len(imagePatches)):
+		image_path = imagePatches[i]
+		file=image_path.split(os.path.sep)[-1]
+		label=int(file[-5:-4])
+		
+		img = cv2.imread(image_path)
+		images.append(img)
+		labels.append(label)
+		
+		if i % 1000 == 0:
+			print(i)
+	return images, labels
+		
+	
 
 
-network = NN()
+#base = r'D:/breast cancer dataset/'
+def Build_model():
+	imagePatches = glob(r"C:\Users\Student\OneDrive\Downloads\archive (4)\**\*.png" , recursive=True)
+	print("imgs:"+ str(len(imagePatches)))
+	for filename in imagePatches[0:10]:
+		print(filename)
+	data0 = []
+	data1 = []
+	for filename in imagePatches:
+		if filename.endswith("class0.png"):
+			data0.append(filename)
+		else:
+			data1.append(filename)
+	print("data0:"+ str(len(data0)))
+	print("data1:"+ str(len(data1)))
+	sampled_data0 = random.sample(data0, 78786)		
+	sampled_data1 = random.sample(data1, 78786)
+	print("data0:"+ str(len(sampled_data0)))
+		
+"""
 
+#network = NN()
+"""
 
 def main():
-	print("hello")
+	# Build_model()
+	x_train, y_train = load_data(r'D:\data\training')
+	network = NN()
+	network.model.fit(x_train, y_train)
+	
 
-if __name__== '_main_':
-	main()
+main()
+
+#if __name__== '_main_':
+#	main()
 
 
